1d2ed04143046c6c51ae6cf43cb63bedbd27f22a
Jackson <jackson.cutler@gmail.com>
2020-06-09 09:44:57 -0700

    Game: Stub over? and tie? methods.

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,27 @@
 require_relative 'board'
 
+class FakeBoard
+  def full?
+  end
+end
+
 class Game
 
   attr_accessor :board
 
-  def initialize(board=nil)
+  def initialize(board=FakeBoard.new)
     @board = board
   end
 
   def over?
+    @board.full?
+  end
+
+  def tie?
+    true
+  end
+
+  def winner
   end
 
 end
