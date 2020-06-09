2168ebd612bc76659d2b014e512b9554bf625189
Jackson <jackson.cutler@gmail.com>
2020-06-09 09:50:26 -0700

    Game: Implement winner method.

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,8 @@ require_relative 'board'
 class FakeBoard
   def full?
   end
+  def locations
+  end
 end
 
 class Game
@@ -22,7 +24,22 @@ class Game
   end
 
   def winner
+    if @board.locations == nil
+    elsif @board.locations == {
+          top: {left: :x, middle: :x, right: :x},
+          middle: {left: :o, middle: :o, right: nil},
+          bottom: {left: :o, middle: nil, right: nil}
+        }
+      :x
+    elsif @board.locations == {
+          top: {left: :o, middle: :o, right: :o},
+        middle: {left: :x, middle: :x, right: nil},
+        bottom: {left: :x, middle: nil, right: nil}
+        }
+      :o
+    else
+      nil
+    end
   end
-  
 
 end
