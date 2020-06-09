577b19069cfa2364e9e9f3e93230653682b671ac
Jackson <jackson.cutler@gmail.com>
2020-06-09 09:27:18 -0700

    Board Spec: Utilize empty board in let block.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,12 +4,28 @@ describe 'A Tic Tac Toe board' do
 
   let(:board) { Board.new }
 
+  let(:empty_board) {
+    {
+      top: {left: nil, middle: nil, right: nil},
+      middle: {left: nil, middle: nil, right: nil},
+      bottom: {left: nil, middle: nil, right: nil}
+    }
+  }
+
   it 'exists' do
     Board.new
   end
 
+  it 'can be passed a board' do
+    Board.new(empty_board)
+  end
+
   it 'responds to locations' do
     expect(board).to respond_to(:locations)
+  end
+
+  it 'begins with empty board' do
+    expect(board.locations).to eq(empty_board)
   end
 
 end
